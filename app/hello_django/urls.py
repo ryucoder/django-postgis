b4735df81de55d7707d512a744d68b2e1975b1da
RyuCoder <rockstar.ryucoder@gmail.com>
2020-03-13 02:17:58 +0530

    Black Formatted.

--- app/hello_django/urls.py
+++ app/hello_django/urls.py
@@ -35,5 +35,3 @@ urlpatterns = [
 
 if bool(settings.DEBUG):
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-    
--- app/upload/views.py
+++ app/upload/views.py
@@ -9,7 +9,5 @@ def image_upload(request):
         filename = fs.save(image_file.name, image_file)
         image_url = fs.url(filename)
         print(image_url)
-        return render(request, "upload.html", {
-            "image_url": image_url
-        })
+        return render(request, "upload.html", {"image_url": image_url})
     return render(request, "upload.html")
